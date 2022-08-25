fe19d90834fdfe81990b564fcb7c16823e790221
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-08-25 01:43:18 -0300

    commit25-08-2022

--- Faculdade/src/main/java/com/asl/faculdade/classeConstrutor/AppCarro.java
+++ Faculdade/src/main/java/com/asl/faculdade/classeConstrutor/AppCarro.java
@@ -9,34 +9,35 @@ package com.asl.faculdade.classeConstrutor;
  * @author java
  */
 public class AppCarro {
-     public static void main(String[] args) {
+	public static void main(String[] args) {
+		
 
-        Carro c1 = new Carro();
-        Carro c2 = new Carro();
-        Carro c3 = new Carro();
+		Carro c1 = new Carro();
+		Carro c2 = new Carro();
+		Carro c3 = new Carro();
 
-        c1.setNome("Gol");
-        c1.setModelo("VolksWagem");
-        c1.setCor("prata");
-        c1.setAno(2006);
+		c1.setNome("Gol");
+		c1.setModelo("VolksWagem");
+		c1.setCor("prata");
+		c1.setAno(2006);
 
-        c2.setNome("Cruise");
-        c2.setModelo("Chevrolet");
-        c2.setCor("preta");
-        c2.setAno(2017);
+		c2.setNome("Cruise");
+		c2.setModelo("Chevrolet");
+		c2.setCor("preta");
+		c2.setAno(2017);
 
-        c3.setNome("Toro");
-        c3.setModelo("Fiat");
-        c3.setCor("branca");
-        c3.setAno(2022);
+		c3.setNome("Toro");
+		c3.setModelo("Fiat");
+		c3.setCor("branca");
+		c3.setAno(2022);
 
-        c1.imprimir();
-        System.out.println("");
-        c2.imprimir();
-        System.out.println("");
-        c3.imprimir();
-        System.out.println("");
+		c1.imprimir();
+		System.out.println("");
+		c2.imprimir();
+		System.out.println("");
+		c3.imprimir();
+		System.out.println("");
 
-    }
+	}
 
 }
